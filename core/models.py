e0eebe25ba1aa7a8e954126ac0169961e4b88a4c
JoaoMalfatti1 <joao.malfatti1210@gmail.com>
2020-09-13 17:44:13 -0400

    Fazendo insercao de dados , update e delete e alteracao

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+#id não precisa fazer cria automatico
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
@@ -19,3 +20,6 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
